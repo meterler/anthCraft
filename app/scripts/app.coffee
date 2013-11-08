e5db591e1e53d2bcf1c89b32553e9d9a857d412d
ijse <i@ijser.cn>
2013-11-08 20:16:23 +0800

    add angular-ui-bootstrap

--- app/scripts/app.coffee
+++ app/scripts/app.coffee
@@ -1,4 +1,3 @@
-
 angular.module('anthCraftApp', [
   'ngCookies'
   'ngResource'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,22 @@
 'use strict';
 
-angular.module('anthCraftApp')
-  .controller('MainCtrl', function ($http, $scope) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+var mod = angular.module('anthCraftApp', ['ui.bootstrap']);
+
+mod.controller('MainCtrl', function ($http, $scope) {
+	$http.get('/api/awesomeThings').success(function(awesomeThings) {
+		$scope.awesomeThings = awesomeThings;
+	});
+
+	$scope.tabs = [
+		{ title:'Dynamic Title 1', content:'Dynamic content 1' },
+		{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+	];
+
+	$scope.alertMe = function() {
+		setTimeout(function() {
+			console.log('You\'ve selected the alert tab!');
+		});
+	};
+
+	$scope.navType = 'pills';
+});
