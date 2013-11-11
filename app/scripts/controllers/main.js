91eb7f25909607df3875584c4377ae8470770009
ijse <i@ijser.cn>
2013-11-11 15:23:12 +0800

    controller split.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,6 +6,9 @@ mod.controller('MainCtrl', function ($http, $scope) {
 	$http.get('/api/awesomeThings').success(function(awesomeThings) {
 		$scope.awesomeThings = awesomeThings;
 	});
+});
+
+mod.controller('tabset', function($http, $scope) {
 
 	$scope.tabs = [
 		{ title:'Dynamic Title 1', content:'Dynamic content 1' },
